4b8872494750404936a04fc0c50c1c2806d90308
ChickenWithACrown <125937747+ChickenWithACrown@users.noreply.github.com>
2024-01-29 03:45:49 -0500

    setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description='SafeGuardAntivirus is an open-source antivirus solution with an integrated firewall for comprehensive protection against malware threats. The script provides easy customization, proactive threat detection, and quarantine capabilities.',
     author='ChickenWithACrown',
     author_discord_user='chickenwithacrown',
-    url='https://github.com/ChickenWithAcrown/SafeGuardAntivirus',
+    url='https://github.com/ChickenWithACrown/SafeGuardAntivirus',
     packages=find_packages(),
     install_requires=[
         # List dependencies here
