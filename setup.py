b79c45d00acabbb7a25213a3e07dc15951048a21
ChickenWithACrown <125937747+ChickenWithAcrown@users.noreply.github.com>
2024-01-29 03:43:53 -0500

    Set Up

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SafeGuardAntivirus',
+    version='1.0',
+    description='SafeGuardAntivirus is an open-source antivirus solution with an integrated firewall for comprehensive protection against malware threats. The script provides easy customization, proactive threat detection, and quarantine capabilities.',
+    author='ChickenWithACrown',
+    author_discord_user='chickenwithacrown',
+    url='https://github.com/ChickenWithAcrown/SafeGuardAntivirus',
+    packages=find_packages(),
+    install_requires=[
+        # List dependencies here
+    ],
+    entry_points={
+        'console_scripts': [
+            'safeguard = SafeGuardAntivirusWithFirewall:main',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: Unlicense',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
